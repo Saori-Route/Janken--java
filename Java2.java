9282177b3c668824786577c31b5818a161b834c0
Saori-Route <46754527+Saori-Route@users.noreply.github.com>
2019-02-10 00:10:58 +0900

    Add files via upload

---
+++ Java2.java
@@ -0,0 +1,21 @@
+package hallo.java;
+import java.util.Scanner;
+
+public class Java2 {
+	public static void main(String[]args) {
+		java.util.Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();{
+			if(a == 1 && b == 2 )
+				System.out.println("xの勝ち");
+			else if(a == 1 && b== 3 )
+				System.out.println("yの勝ち");
+			else if(a == 1 && b == 1)
+				System.out.println("xとyはあいこ");
+			else
+				System.out.println("じゃんけんしましょう");
+
+		}
+
+	}
+}
