d0c95ba23c89285fde37972c7856f193e58f3675
Saori-Route <46754527+Saori-Route@users.noreply.github.com>
2019-02-10 02:01:41 +0900

    Update Java2.java

--- Java2.java
+++ Java2.java
@@ -5,17 +5,29 @@ public class Java2 {
 	public static void main(String[]args) {
 		java.util.Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
-		int b = sc.nextInt();{
-			if(a == 1 && b == 2 )
-				System.out.println("xの勝ち");
-			else if(a == 1 && b== 3 )
-				System.out.println("yの勝ち");
-			else if(a == 1 && b == 1)
-				System.out.println("xとyはあいこ");
-			else
-				System.out.println("じゃんけんしましょう");
+		int b = sc.nextInt();
+		if(a == 1 && b == 2 )
+			System.out.println("xの勝ち");
+		else if(a == 1 && b== 3 )
+			System.out.println("yの勝ち");
+		else if(a == 1 && b == 1)
+			System.out.println("xとyはあいこ");
+		else if(a == 2 && b == 1)
+			System.out.println("yの勝ち");
+		else if(a == 2 && b == 3)
+			System.out.println("xの勝ち");
+		else if(a == 2 && b == 2)
+			System.out.println("xとyはあいこ");
+		else if(a == 3 && b == 1)
+			System.out.println("xの勝ち");
+		else if(a == 3 && b == 2)
+			System.out.println("yの勝ち");
+		else if(a == 3 && b == 3)
+			System.out.println("xとyはあいこ");
+		else
+			System.out.println("じゃんけんしましょう");
 
-		}
+		
 
 	}
 }
